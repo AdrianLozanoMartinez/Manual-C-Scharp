4adacdf322794d1e6fb07d963a323bcd37f79a4e
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-10-14 13:00:24 +0200

    Dotnet

---
+++ Dotnet/Dotnet.cs
@@ -0,0 +1,14 @@
+﻿/* Instalar SDK    ->    https://dotnet.microsoft.com/en-us/download/dotnet/6.0
+ 
+ * Accedemos PowerShell para desarrolladores    ->    Ctrl + ñ
+ 
+ * El proyecto debe estar en    ->    C:\Users\NombreUsuario\source\repos
+    - dotnet --version   ->   Sale la versión. 6.0.401
+    - dotnet --info      ->   Información
+    - dotnet --help      ->   Explica comandos y opciones
+    
+    - Dentro de cada proyecto  ->  Ej: ManualCSharp/Dotnet
+        > dotnet run     ->   Se ejecuta el código. Ejemplo: Console.WriteLine("Hello, World!");
+ */
+
+Console.WriteLine("Hello, World!");
--- ManualCSharp/Program.cs
+++ ManualCSharp/Program.cs
@@ -1,9 +1,4 @@
 ﻿/*
  * Necesario SDK -> https://dotnet.microsoft.com/en-us/download/dotnet/6.0 u otro
  * Sirve: Windows store app, Windows Client, Office, Components, Enterprise, Website, Mobile Apps, Backend Service, Game
- * Ctrl + ñ -> PowerShell para desarrolladores
- * Terminal -> Comando: dotnet
-    - dotnet --version -> Sale la versión. 6.0.401
-    - dotnet --info -> Información
-    - dotnet --help -> Explica comandos y opciones
- */ 
+*/

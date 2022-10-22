f05368e5d37189dd0be2185d58dba33eadfd5551
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-10-22 09:33:47 +0200

    Planteamiento ejercicios

---
+++ xEjercicio18/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xEjercicio18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ xEjercicio22/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xEjercicio22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

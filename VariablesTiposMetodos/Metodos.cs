6dc168f1fd4a9bd2b4f0c805c8e95db84ba27fb2
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-10-30 09:14:53 +0100

    split y codingame

--- VariablesTiposMetodos/Metodos.cs
+++ VariablesTiposMetodos/Metodos.cs
@@ -13,7 +13,7 @@
 
              _Ejemplo:                                                                                                                                 */
              Console.WriteLine("Nos da la letra de la posición [nº] elegida de \"Hola[1]\": " + "Hola"[1]); //Sale o.  0=H, 1=o, 2=l, 3=a
-                                                                                                         //Posición: 1º   2º   3º   4º
+                                                                                                            //Posición: 1º   2º   3º   4º
                                                                                                                                                                         /*                                                                                                                                                   /*
     - Reemplazar
         > variable_texto/texto.Replace(variable/elemento, variable/elemento);
@@ -22,10 +22,27 @@
              string variable = "23.4".Replace(".", ",");
              Console.WriteLine(variable); //Sale 23,4
                                                                                                                                                             /*
-    - Eliminar/Reemplazar un caracter
-       > Eliminar -> texto/variable.split('Caracter') 
+    - Eliminar un/más caracter
+       > Uno -> texto/variable.split('Caracter') 
          _Ejemplo                                                                                                                                                   */
-                                                                                                                                                                                     /*
-       > Eliminar -> texto/variable.split('Caracter')
-         _Ejemplo                                                                                                                                                                           
-                                                                                                                                                              */
+         string texto = ".Hola. .bienvenido. .aquí.";
+         string[] array = texto.Split('.'); //Eliminamos los .
+        
+         Console.WriteLine($"Resultado de quitar los . en:\n.Hola. .bienvenido. .aquí.: ");
+         foreach (var variableLocal in array)
+         {
+            Console.Write($"{variableLocal}");
+         }
+          Console.WriteLine('\n');                                                                                                                                  /*
+
+       > Más -> texto/variable.split('Caracter', 'Caracter', 'Caracter' ...)
+         _Ejemplo                                                                                                                                           */
+        string texto2 = ".Hola. .bienvenido. .aquí.";
+        string[] array2 = texto2.Split('.', 'a', 'e', 'i', 'í', 'o', 'u'); //Eliminamos los . y vocales
+
+        Console.WriteLine($"Resultado de quitar los . y vocales en:\n.Hola. .bienvenido. .aquí.: ");
+        foreach (var variableLocal2 in array2)
+        {
+            Console.Write($"{variableLocal2}");
+        }
+        Console.WriteLine();
---
+++ xEjerciciosCodingameDirectorios/Program.cs
@@ -0,0 +1,21 @@
+﻿class xEjerciciosCodingameDirectorios
+{
+    static void Main(string[] args)
+    {
+        int _num = int.Parse(Console.ReadLine());
+        string prueba = "";
+        for (int i = 0; i < _num; i++)
+        {
+            string folder = Console.ReadLine();
+            prueba += folder + "/";
+        }
+
+        string filename = Console.ReadLine();
+        string extension = Console.ReadLine();
+
+        // prueba += filename + '.' + extension;  //otra forma
+        string result = prueba + filename + "." + extension;
+        Console.WriteLine(result);
+    }
+}
+

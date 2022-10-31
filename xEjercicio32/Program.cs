d1ac9dd6cc7af437bd5ea2945e2e174079727632
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-10-31 18:14:29 +0100

    Ejercicios array por el profe

---
+++ xEjercicio32/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace xEjercicio32
+{
+    internal class Program
+    {
+        /*
+         Ejercicio 32.
+         Crea un array de 10 posiciones de números con valores pedidos por teclado. Muestra por
+         consola el índice y el valor al que corresponde. Haz dos métodos, uno para rellenar valores
+         y otro para mostrar.
+         */
+
+        static void Main(string[] args)
+        {/*//Sin metodos
+            int[] numbers = new int[10];
+
+            Console.WriteLine("Intr");
+            //Meter valores
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            //Mostrar valores
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine($"Índice: {i} Valor: {numbers[i]}");
+            }*/
+        }
+
+        
+    }
+}

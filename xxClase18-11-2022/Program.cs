47a3638e65bf23a44b891f98edc0af7fecaa9ed5
Adrián Lozano Martínez <adrianlm1985@gmail.com>
2022-11-18 12:44:17 +0100

    Ejercicios

---
+++ xxClase18-11-2022/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace xxClase18_11_2022
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
